fab555be3fd99fa3431bbb7dcd0eacfd8afd5c5a
Tiến Lê <ww.hacker01@gmail.com>
2023-12-28 12:06:23 +0700

    Update commit

--- src/main/java/Tool/APIControllers/ToolAPIController.java
+++ src/main/java/Tool/APIControllers/ToolAPIController.java
@@ -26,7 +26,8 @@ import Tool.Models.ResponseModel;
 public class ToolAPIController {
 
 	@PostMapping(path = "/register")
-	public ResponseEntity<?> register(@Validated @RequestBody String jsonData,
+	public ResponseEntity<?> register(
+			@Validated @RequestBody String jsonData,
 			@Validated @RequestHeader(name = HttpHeaders.AUTHORIZATION, required = true) String authorizationHeader) {
 		LocalDateTime now = LocalDateTime.now();
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SS");
@@ -39,24 +40,17 @@ public class ToolAPIController {
 			URL apiUrl = new URL(url);
 			HttpURLConnection connection = (HttpURLConnection) apiUrl.openConnection();
 
-			// Set request method
 			connection.setRequestMethod("POST");
-			// Set request headers
 			connection.setRequestProperty("Content-Type", "application/json");
-			// Set token authentication
 			connection.setRequestProperty("Authorization", "Bearer " + token);
-			// Enable input/output streams
 			connection.setDoOutput(true);
 
-			// Write JSON data to output stream
 			try (OutputStream os = connection.getOutputStream()) {
 				byte[] input = jsonData.getBytes(StandardCharsets.UTF_8);
 				os.write(input, 0, input.length);
 			}
 
-			// Get HTTP response code
 			int responseCode = connection.getResponseCode();
-			// Read the response from the server
 			BufferedReader br = new BufferedReader(
 					new InputStreamReader(connection.getInputStream(), StandardCharsets.UTF_8));
 
@@ -66,15 +60,12 @@ public class ToolAPIController {
 				responseStringBuilder.append(line);
 			}
 
-			// Handle the response based on your requirements
 			String responseBody = responseStringBuilder.toString();
-			// Close the connection
 			connection.disconnect();
-			// Return ResponseEntity based on the response code
+			
 			return new ResponseEntity<>(new ResponseModel(true, "Success", responseBody, formattedDateTime),
 					HttpStatus.resolve(responseCode));
 		} catch (Exception e) {
-			// e.printStackTrace();
 			return new ResponseEntity<>(new ResponseModel(false, "Error", "{}", formattedDateTime),
 					HttpStatus.INTERNAL_SERVER_ERROR);
 		}
--- src/main/java/Tool/Controllers/RegisterController.java
+++ src/main/java/Tool/Controllers/RegisterController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class RegisterController {
 
-	@GetMapping("/tool")
+	@GetMapping(value = {"/", "/tool"})
 	public String showFormRegister() {
 		return "register";
 	}
